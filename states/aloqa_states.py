f24aa9473831b9ef58b0d7602b7f6362878cf6a9
beha7080 <tasmatovbehruz72@gmail.com>
2024-11-09 15:13:11 +0500

    update 1

---
+++ states/aloqa_states.py
@@ -0,0 +1,7 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+class BotStates(StatesGroup):
+    reklama_state = State()
